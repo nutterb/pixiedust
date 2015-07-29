1ec285f825664979be260e7657a640fc9f49ca5b
Benjamin <benjamin.nutter@gmail.com>
2015-07-29 07:16:42 -0400

    Update sprinkles.R

--- R/sprinkles.R
+++ R/sprinkles.R
@@ -31,30 +31,65 @@
 #' \code{...} dots argument.
 #' 
 #' \itemize{
-#'   \item{\code{bg} }{}
-#'   \item{\code{bg_pattern }{}
-#'   \item{\code{bold }{}
-#'   \item{\code{border_collapse }{}
-#'   \item{\code{fn }{}
-#'   \item{\code{font_color }{}
-#'   \item{\code{font_size }{}
-#'   \item{\code{halign }{}
-#'   \item{\code{height }{}
-#'   \item{\code{italic }{}
-#'   \item{\code{pad }{}
-#'   \item{\code{rotate_text }{}
-#'   \item{\code{round }{}
-#'   \item{\code{valign }{}
-#'   \item{\code{width }{}
+#'   \item{\code{bg} }{A character string denoting the color 
+#'      for the background.  See "Colors".}
+#'   \item{\code{bg_pattern} }{This is one of the few exceptions to the length 1 rule.
+#'      This accepts a vector of any length.  Background colors are recycled in a 
+#'      pattern. See "Colors"}
+#'   \item{\code{bg_pattern_by} }{A character string denoting if the background 
+#'      pattern is recycled over rows or columns.  Accepts either \code{"rows"},
+#'      or \code{"columns"} with partial matching and defaults to \code{"rows"}.
+#'      if \code{bg_pattern} is provided, \code{bg_pattern_by} is assumed, meaning
+#'      it is not necessary to explicitly define \code{bg_pattern_by} unless 
+#'      changing an existing or default setting.}
+#'   \item{\code{bold} }{}
+#'   \item{\code{border} }{This is one of the few exceptions to the length 1 rule.  
+#'      Accepts values \code{"left"}, \code{"right"}, \code{"top"}, and
+#'      \code{"bottom"} with partial matching.  The border will be added
+#'      to the sides indicated.}
+#'   \item{\code{border_thickness} }{A numeric value denoting the thickness
+#'      of the border.  Defaults to \code{1}.}
+#'   \item{\code{border_units} }{A character string taking any one of the
+#'      values \code{"px"} or \code{"pt"} with partial matching.  Defaults
+#'      to \code{"px"}.}
+#'   \item{\code{border_style} }{A character string taking any one of the
+#'      values \code{"solid"}, \code{"dashed"}, \code{"dotted"}, 
+#'      \code{"double"}, \code{"groove"}, \code{"ridge"}, \code{"inset"},
+#'      \code{"outset"}, \code{"hidden"}, or \code{"none"}.  Defaults
+#'      to \code{"solid"}.}
+#'   \item{\code{border_color} }{A character string denoting the color 
+#'      for the border.  See "Colors".}
+#'   \item{\code{border_collapse} }{This element is only applicable to 
+#'      \code{part = "table"} and will be applied to the table regardless
+#'      the value of \code{part} in the call.}
+#'   \item{\code{fn} }{}
+#'   \item{\code{font_color} }{}
+#'   \item{\code{font_size} }{}
+#'   \item{\code{font_size_units} }{A character string giving the units 
+#'     of the font size.  Accepts values \code{"px"}, \code{"pt"}, \code{"\%"},
+#'     and \code{"em"}.  Defaults to \code{"px"}.}
+#'   \item{\code{halign} }{}
+#'   \item{\code{height} }{}
+#'   \item{\code{height_units} }{}
+#'   \item{\code{italic} }{}
+#'   \item{\code{pad} }{}
+#'   \item{\code{rotate_text} }{}
+#'   \item{\code{round} }{}
+#'   \item{\code{valign} }{}
+#'   \item{\code{width} }{}
+#'   \item{\code{width_units} }{}
 #' }
+#'
+#' @section Colors:
+#' Color specifications accept X11 color names (\code{"orchid"}), 
+#' hexidecimal names (\code{"#DA70D6"}), rgb names (\code{"rgb(218 112 214)"}),
+#' and rgba (rgb+alpha transparancy; \code{"rgba(218, 112, 214, .75)"}).
+#' Refer to \url{https://en.wikipedia.org/wiki/Web_colors#X11_color_names}.
+#'
+#' @seealso \code{\link{sprinkle_colnames}} for changing column names in a table.
 #' 
 #' @author Benjamin Nutter
 #'    
-c("bg", "bg_pattern", "bold", "border_collapse",
-  "fn", "font_color", "font_size", "halign", 
-  "height", "italic", "pad", "rotate_text", 
-  "round", "valign", "width")
-
 sprinkle <- function(rows, cols, ..., 
                      part = c("body", "head", "foot", "interfoot", "table"))
 {
@@ -100,6 +135,14 @@ sprinkle <- function(rows, cols, ...,
             class = "sprinkle")
 }
 
+#' @rdname sprinkles
+#' @param print_method A character string giving the print method for the table.  
+#' @export
+
+sprinkle_print_method <- function(print_method = c("console", "markdown", "html", "latex"))
+{
+  
+}
 sprinkle_names <- function()
 {
   c("bg", "bg_pattern", "bold", "border_collapse",
