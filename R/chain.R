32ac4583328d3dfba045627b9bb4aadbd4c47f66
alex gable <alex.gable@gmail.com>
2021-01-03 23:56:26 -0800

    re-add chain, using poorman

---
+++ R/chain.R
@@ -0,0 +1,15 @@
+#' @name %>%
+#' @rdname chain
+#' @importFrom poorman %>%
+#' @export %>%
+#' @usage lhs \%>\% rhs
+#'
+#' @title Chain together multiple operations.
+#' @description This is a copy of the documentation for \code{\%>\%} in
+#' \code{poorman}.  The copy here is made to conform to CRAN requirements
+#' regarding documentation.  Please see the \code{poorman} documenation
+#' for the complete and current documentation.
+#'
+#' @param lhs,rhs A dataset and function to apply to it
+
+NULL
